0c520ce9bc56a3afb7bb7b9bccd152a394b836c8
Brian Andere <briancollins081@gmail.com>
2021-05-27 21:54:23 +0300

    Corrected the configuration for the reactive base connection headers

--- src/ex_one/ExampleOne.js
+++ src/ex_one/ExampleOne.js
@@ -2,26 +2,35 @@
 import { DataSearch, ReactiveBase } from "@appbaseio/reactivesearch";
 
 const ExampleOne = () => {
+  const onData = (searchItem) => {
+    // Check data and extract your information
+  }
   return (
-    <div>
+    <div className="container-fluid">
       <ReactiveBase
-        app="articles,counsellors,events,forums,groupsopen,groupsprivate,mentees,mentors,organizations,speakers,timelineposts,tstoreproducts,tstoreservices,twaabooks,users,videos"
+        // app="articles,counsellors,events,forums,groupsopen,groupsprivate,mentees,mentors,organizations,speakers,timelineposts,tstoreproducts,tstoreservices,twaabooks,users,videos"
+        app="articles"
         url={`http://127.0.0.1:9200`}
-        credentials={`${process.env.ELASTIC_USER_NAME}:${process.env.ELASTIC_USER_PASSWORD}`}
+        // credentials={`${process.env.REACT_APP_ELASTIC_USER_NAME}:${process.env.REACT_APP_ELASTIC_USER_PASSWORD}`}
+        // credentials={"elastic:bR4GQWvXFpL8tPrD9Jjd"}
         // headers={{
         //   Authorization:
         //     "T3hiV3JYa0JKMmNkX1pXMm83dEY6dUJNX28wY3RRR2VlMUh0VFp3OU8tdw==",
         // }}
-        transformRequest={(request) => ({
-          ...request,
-          headers: {
-            ...request.headers,
-            Authorization:
-              "Basic T3hiV3JYa0JKMmNkX1pXMm83dEY6dUJNX28wY3RRR2VlMUh0VFp3OU8tdw==",
-          },
-        })}
+        transformRequest={(request) => {
+          let headers = request.headers;
+          console.log({ headers });
+          // headers.Authorization = `Basic ${process.env.REACT_APP_ELASTIC_API_KEY}`;
+          headers.Authorization = `ApiKey ${process.env.REACT_APP_ELASTIC_API_KEY}`;
+          return {
+            ...request,
+            headers: {
+              ...headers,
+            },
+          };
+        }}
       >
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
           <a className="navbar-brand" href="#">
             Yelp Search
           </a>
@@ -52,11 +61,31 @@ const ExampleOne = () => {
                     <br />
                     Error details
                     <br />
-                    {error}
+                    {JSON.stringify(error)}
                   </div>
                 )}
               />
             </div>
+          </div>
+          <div className="links">
+            <a
+              target="_blank"
+              href="https://github.com/appbaseio/reactivesearch"
+              className="btn text-primary"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github"></i>&nbsp; Github
+            </a>
+            &nbsp;&nbsp;
+            <a
+              target="_blank"
+              href="https://opensource.appbase.io/reactive-manual/"
+              className="btn text-primary"
+              rel="noreferrer"
+            >
+              <i className="fas fa-book" aria-hidden="true" />
+              &nbsp;Documentation
+            </a>
           </div>
         </nav>
       </ReactiveBase>
