b7f0eacd0d9a9bf5140a0eeff8e5ce0e0e58d2d7
Brian Andere <briancollins081@gmail.com>
2021-05-28 22:31:25 +0300

    Updates to git ignore

--- src/ex_one/ExampleOne.js
+++ src/ex_one/ExampleOne.js
@@ -244,7 +244,9 @@ const ExampleOne = () => {
               )}
             />
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-6" />
+          <div className="col-lg-3 col-md-3 col-sm-6">
+            
+          </div>
         </div>
       </ReactiveBase>
     </div>
