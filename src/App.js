cde9bf4a3f4289f4410c2c002517e1b8854a61b9
Brian Andere <briancollins081@gmail.com>
2021-06-02 11:43:08 +0300

    Updated TWAA Search version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ExampleOne from "./ex_one/ExampleOne";
+import SearchWrapper from "./components/SearchWrapper";
 
 function App() {
   return (
     <main className="container-fluid">
-      <div className="d-flex flex-column justify-items-center align-items-center py-5 w-100">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="fs-3 fw-bold font-monospace text-decoration-underline">Learning Reactive Search</h1>
-        <hr />
-        <ExampleOne />
-      </div>
+        <SearchWrapper />
     </main>
   );
 }
